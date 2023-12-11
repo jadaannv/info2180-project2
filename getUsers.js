53fc0752ea44ff0100f43ebc5def16e1a04f5ecb
jadaannv <71792039+jadaannv@users.noreply.github.com>
2023-12-10 19:27:23 -0500

    Updated getUsers

--- getUsers.js
+++ getUsers.js
@@ -1,4 +1,13 @@
 function starter(){
+
+    //add functionality go add user button
+    let addBtn = document.getElementById('addUser');
+    addBtn.addEventListener("click", function(e){
+        e.preventDefault();
+        window.location.href = "newUser.php";
+    })
+    
+    //display list of users for an admin
     let url = 'userList.php'
 
     var request = new XMLHttpRequest();
